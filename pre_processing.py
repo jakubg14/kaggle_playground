924d2c32e1443f5b8c6386ee78d3a7134af6b232
jakubg14 <jimmy.grzybek@gmail.com>
2017-06-17 00:53:11 -0400

    Check for missing data

--- pre_processing.py
+++ pre_processing.py
@@ -7,10 +7,31 @@ def remove_unnamed_columns(data_frame):
 		if 'Unnamed' in column:
 			del data_frame[column]
 
+def check_missing_vals(column):
+	missing_entries = []
+	for counter, val in enumerate(column):
+		if val is None or val == '' or val == '-':
+			missing_entries.append(counter)
+	return missing_entries
+
+def find_poor_observation(dataframe):
+	errors = []
+	for column in list(dataframe):
+		errors = errors + check_missing_vals(column)
+		if len(errors) > 0:
+			print(str(column) + ' has missing value')
+	print(str(len(errors)) + ' poor observations were found.')
+	return set(errors)
+		
 data = pd.read_csv('data.csv', encoding='utf-8')
 
 remove_unnamed_columns(data)
 
+errors = find_poor_observation(data)
+
+
 data['radius_mean'].plot()
 
 plt.show()
+
+
